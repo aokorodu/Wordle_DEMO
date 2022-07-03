1692eae513ce8dcc6d5191abe165c4146420997f
aokorodu <aokorodu@gmail.com>
2022-07-03 14:35:30 -0400

    added ability to change length of words, minor styling update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Wordle from "./components/Wordle";
 
 function App() {
   const [max, setMax] = useState(6);
+  const [length, setLength] = useState(5);
   const [start, setStart] = useState(false);
 
   const getWord = () => {
-    let arr = [
+    let arr_5 = [
       "Jazzy",
       "Quack",
       "Jumbo",
@@ -29,6 +30,55 @@ function App() {
       "Xenon",
       "Yacht",
     ];
+    let arr_3 = [
+      "ham",
+      "jam",
+      "cap",
+      "red",
+      "get",
+      "goy",
+      "gal",
+      "guy",
+      "gin",
+      "gun",
+      "bae",
+      "dog",
+      "map",
+      "nil",
+      "cob",
+      "caw",
+      "aim",
+      "Mad",
+      "Hog",
+      "Pay",
+    ];
+
+    let arr_4 = [
+      "Bake",
+      "Word",
+      "List",
+      "Four",
+      "Five",
+      "Nine",
+      "Good",
+      "Best",
+      "Cute",
+      "Zero",
+      "Huge",
+      "Cool",
+      "Tree",
+      "Race",
+      "Rice",
+      "Keep",
+      "Lace",
+      "Beam",
+      "Game",
+      "Mars",
+    ];
+
+    let arr = arr_5
+    if(length == 3) arr = arr_3;
+    if(length == 4) arr = arr_4;
     const num = arr.length;
     const ind = Math.floor(Math.random() * num);
 
@@ -41,6 +91,12 @@ function App() {
     setMax(e.target.value);
   };
 
+  const updateLength = (e) => {
+    const newVal = e.target.value;
+    console.log("new value", newVal);
+    setLength(e.target.value);
+  };
+
   const beginGame = () => {
     setStart(true);
   };
@@ -50,11 +106,12 @@ function App() {
       {start && <Wordle newWord={getWord()} attempts={max} />}
       {!start && (
         <div className="settingsHolder">
+          <div>WORDL DEMO</div>
           <div className="attemptHolder">
             <div>
               <span>Max Attempts</span>
               <select
-              className="settingsSelect"
+                className="settingsSelect"
                 name="attempts"
                 onChange={updateAttempts}
                 defaultValue="6"
@@ -63,9 +120,26 @@ function App() {
                 <option value="6">6</option>
                 <option value="7">7</option>
                 <option value="8">8</option>
-                <option value="9">9</option>
               </select>
             </div>
+
+
+
+            <div>
+              <span>Word Length</span>
+              <select
+                className="settingsSelect"
+                name="length"
+                onChange={updateLength}
+                defaultValue="5"
+              >
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+
+
 
             <button onClick={beginGame}>BEGIN</button>
           </div>
