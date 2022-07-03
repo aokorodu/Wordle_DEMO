d5e4bf629a479d6a47936c0541c9fb0c986f1548
aokorodu <aokorodu@gmail.com>
2022-07-03 12:35:27 -0400

    updated keyboard with bigger keys

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,474 +2,368 @@ import React from "react";
 import "./Keyboard.css";
 
 class Keyboard extends React.Component {
-  constructor({onKeyPress}) {
+  constructor({ onKeyPress }) {
     super();
     this.keys = null;
     this.onKeyPress = onKeyPress;
   }
 
   componentDidMount() {
-    console.log('Keyboard componentDidMount-------------')
+    console.log("Keyboard componentDidMount-------------");
     this.keys = document.querySelectorAll(".key");
     console.log("keys:", this.keys.length);
-    this.keys.forEach((key)=>{
-      
-      key.addEventListener("click", ()=>{
+    this.keys.forEach((key) => {
+      key.addEventListener("click", () => {
         console.log(key.getAttribute("id"));
         this.onKeyPress(key.getAttribute("id"));
-      })
-    })
+      });
+    });
   }
 
   render() {
     return (
       <div>
-        <svg width="400" height="160" viewBox="0 0 390 140">
+        <svg width="350" height="160" viewBox="0 0 336 144">
           <g id="keyboard">
-            <g className="key" id="Q">
-              <rect
-                id="Rectangle 1"
-                x="27"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="Q">
+              <rect id="Rectangle 1" width="30" height="45" rx="5" />
               <text id="Q_2" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="36.1797" y="40.3182">
+                <tspan x="9.17969" y="29.6364">
                   Q
                 </tspan>
               </text>
             </g>
-            <g className="key" id="W">
-              <rect
-                id="Rectangle 1_2"
-                x="61"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="W">
+              <rect id="Rectangle 1_2" x="34" width="30" height="45" rx="5" />
               <text id="Q_3" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="67.4844" y="40.3182">
+                <tspan x="40.9844" y="29.6364">
                   W
                 </tspan>
               </text>
             </g>
-            <g className="key" id="A">
+            <g className="key" fill="#D9D9D9" id="A">
               <rect
                 id="Rectangle 1_3"
-                x="43"
-                y="54"
+                x="17"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_4" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="52.3281" y="75.3182">
+                <tspan x="26.3281" y="78.6364">
                   A
                 </tspan>
               </text>
             </g>
-            <g className="key" id="E">
-              <rect
-                id="Rectangle 1_4"
-                x="95"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="E">
+              <rect id="Rectangle 1_4" x="68" width="30" height="45" rx="5" />
               <text id="Q_5" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="105.062" y="40.3182">
+                <tspan x="78.5625" y="29.6364">
                   E
                 </tspan>
               </text>
             </g>
-            <g className="key" id="S">
+            <g className="key" fill="#D9D9D9" id="S">
               <rect
                 id="Rectangle 1_5"
-                x="77"
-                y="54"
+                x="51"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_6" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="87.2188" y="75.3182">
+                <tspan x="61.2188" y="78.6364">
                   S
                 </tspan>
               </text>
             </g>
-            <g className="key" id="Z">
+            <g className="key" fill="#D9D9D9" id="Z">
               <rect
                 id="Rectangle 1_6"
-                x="77"
-                y="89"
+                x="50"
+                y="98"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_7" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="87.0547" y="110.318">
+                <tspan x="60.0547" y="127.636">
                   Z
                 </tspan>
               </text>
             </g>
-            <g className="key" id="R">
-              <rect
-                id="Rectangle 1_7"
-                x="129"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="R">
+              <rect id="Rectangle 1_7" x="102" width="30" height="45" rx="5" />
               <text id="Q_8" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="139.203" y="40.3182">
+                <tspan x="112.203" y="29.6364">
                   R
                 </tspan>
               </text>
             </g>
-            <g className="key" id="D">
+            <g className="key" fill="#D9D9D9" id="D">
               <rect
                 id="Rectangle 1_8"
-                x="111"
-                y="54"
+                x="85"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_9" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="120.156" y="75.3182">
+                <tspan x="94.6562" y="78.6364">
                   D
                 </tspan>
               </text>
             </g>
-            <g className="key" id="X">
+            <g className="key" fill="#D9D9D9" id="X">
               <rect
                 id="Rectangle 1_9"
-                x="111"
-                y="89"
+                x="84"
+                y="98"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_10" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="120.094" y="110.318">
+                <tspan x="93.5938" y="127.636">
                   X
                 </tspan>
               </text>
             </g>
-            <g className="key" id="T">
-              <rect
-                id="Rectangle 1_10"
-                x="163"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="T">
+              <rect id="Rectangle 1_10" x="136" width="30" height="45" rx="5" />
               <text id="Q_11" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="173.086" y="40.3182">
+                <tspan x="146.086" y="29.6364">
                   T
                 </tspan>
               </text>
             </g>
-            <g className="key" id="F">
+            <g className="key" fill="#D9D9D9" id="F">
               <rect
                 id="Rectangle 1_11"
-                x="145"
-                y="54"
+                x="119"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_12" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="155.32" y="75.3182">
+                <tspan x="129.82" y="78.6364">
                   F
                 </tspan>
               </text>
             </g>
-            <g className="key" id="C">
+            <g className="key" fill="#D9D9D9" id="C">
               <rect
                 id="Rectangle 1_12"
-                x="145"
-                y="89"
+                x="118"
+                y="98"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_13" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="154.414" y="110.318">
+                <tspan x="127.414" y="127.636">
                   C
                 </tspan>
               </text>
             </g>
-            <g className="key" id="Y">
-              <rect
-                id="Rectangle 1_13"
-                x="197"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="Y">
+              <rect id="Rectangle 1_13" x="170" width="30" height="45" rx="5" />
               <text id="Q_14" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="206.047" y="40.3182">
+                <tspan x="179.547" y="29.6364">
                   Y
                 </tspan>
               </text>
             </g>
-            <g className="key" id="G">
+            <g className="key" fill="#D9D9D9" id="G">
               <rect
                 id="Rectangle 1_14"
-                x="179"
-                y="54"
+                x="153"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_15" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="188.367" y="75.3182">
+                <tspan x="162.367" y="78.6364">
                   G
                 </tspan>
               </text>
             </g>
-            <g className="key" id="V">
+            <g className="key" fill="#D9D9D9" id="V">
               <rect
                 id="Rectangle 1_15"
-                x="179"
-                y="89"
+                x="152"
+                y="98"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_16" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="188.328" y="110.318">
+                <tspan x="161.328" y="127.636">
                   V
                 </tspan>
               </text>
             </g>
-            <g className="key" id="U">
-              <rect
-                id="Rectangle 1_16"
-                x="231"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="U">
+              <rect id="Rectangle 1_16" x="204" width="30" height="45" rx="5" />
               <text id="Q_17" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="240.195" y="40.3182">
+                <tspan x="213.695" y="29.6364">
                   U
                 </tspan>
               </text>
             </g>
-            <g className="key" id="H">
+            <g className="key" fill="#D9D9D9" id="H">
               <rect
                 id="Rectangle 1_17"
-                x="213"
-                y="54"
+                x="187"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_18" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="222.016" y="75.3182">
+                <tspan x="196.516" y="78.6364">
                   H
                 </tspan>
               </text>
             </g>
-            <g className="key" id="B">
+            <g className="key" fill="#D9D9D9" id="B">
               <rect
                 id="Rectangle 1_18"
-                x="213"
-                y="89"
+                x="186"
+                y="98"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_19" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="223.188" y="110.318">
+                <tspan x="196.188" y="127.636">
                   B
                 </tspan>
               </text>
             </g>
-            <g className="key" id="I">
-              <rect
-                id="Rectangle 1_19"
-                x="265"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="I">
+              <rect id="Rectangle 1_19" x="238" width="30" height="45" rx="5" />
               <text id="Q_20" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="278.211" y="40.3182">
+                <tspan x="251.211" y="29.6364">
                   I
                 </tspan>
               </text>
             </g>
-            <g className="key" id="J">
+            <g className="key" fill="#D9D9D9" id="J">
               <rect
                 id="Rectangle 1_20"
-                x="247"
-                y="54"
+                x="221"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_21" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="257.367" y="75.3182">
+                <tspan x="231.867" y="78.6364">
                   J
                 </tspan>
               </text>
             </g>
-            <g className="key" id="N">
+            <g className="key" fill="#D9D9D9" id="N">
               <rect
                 id="Rectangle 1_21"
-                x="247"
-                y="89"
+                x="220"
+                y="98"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_22" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="256.164" y="110.318">
+                <tspan x="229.664" y="127.636">
                   N
                 </tspan>
               </text>
             </g>
-            <g className="key" id="O">
-              <rect
-                id="Rectangle 1_22"
-                x="299"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="O">
+              <rect id="Rectangle 1_22" x="272" width="30" height="45" rx="5" />
               <text id="Q_23" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="308.188" y="40.3182">
+                <tspan x="281.188" y="29.6364">
                   O
                 </tspan>
               </text>
             </g>
-            <g className="key" id="K">
+            <g className="key" fill="#D9D9D9" id="K">
               <rect
                 id="Rectangle 1_23"
-                x="281"
-                y="54"
+                x="255"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_24" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="290.383" y="75.3182">
+                <tspan x="264.883" y="78.6364">
                   K
                 </tspan>
               </text>
             </g>
-            <g className="key" id="M">
+            <g className="key" fill="#D9D9D9" id="M">
               <rect
                 id="Rectangle 1_24"
-                x="281"
-                y="89"
+                x="254"
+                y="98"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_25" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="289.117" y="110.318">
+                <tspan x="262.117" y="127.636">
                   M
                 </tspan>
               </text>
             </g>
-            <g className="key" id="P">
-              <rect
-                id="Rectangle 1_25"
-                x="333"
-                y="19"
-                width="30"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="P">
+              <rect id="Rectangle 1_25" x="306" width="30" height="45" rx="5" />
               <text id="Q_26" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="343.281" y="40.3182">
+                <tspan x="316.281" y="29.6364">
                   P
                 </tspan>
               </text>
             </g>
-            <g className="key" id="L">
+            <g className="key" fill="#D9D9D9" id="L">
               <rect
                 id="Rectangle 1_26"
-                x="315"
-                y="54"
+                x="289"
+                y="49"
                 width="30"
-                height="30"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_27" fill="black" fontSize="16" fontWeight="800">
-                <tspan x="325.445" y="75.3182">
+                <tspan x="299.945" y="78.6364">
                   L
                 </tspan>
               </text>
             </g>
-            <g className="key" id="ENTER">
-              <rect
-                id="Rectangle 1_27"
-                x="27"
-                y="89"
-                width="45"
-                height="30"
-                rx="5"
-                fill="#D9D9D9"
-              />
+            <g className="key" fill="#D9D9D9" id="ENTER">
+              <rect id="Rectangle 1_27" y="98" width="47" height="45" rx="5" />
               <text id="Q_28" fill="black" fontSize="10" fontWeight="600">
-                <tspan x="33.167" y="107.636">
+                <tspan x="7.16699" y="124.636">
                   ENTER
                 </tspan>
               </text>
             </g>
-            <g className="key" id="BACK">
+            <g className="key" fill="#D9D9D9" id="BACK">
               <rect
                 id="Rectangle 1_28"
-                x="318"
-                y="90"
-                width="45"
-                height="30"
+                x="289"
+                y="98"
+                width="47"
+                height="45"
                 rx="5"
-                fill="#D9D9D9"
               />
               <text id="Q_29" fill="black" fontSize="10" fontWeight="600">
-                <tspan x="333.771" y="108.636">
+                <tspan x="305.771" y="124.636">
                   &#60;=
                 </tspan>
               </text>
