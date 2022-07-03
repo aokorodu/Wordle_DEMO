6654598f4889f02da4bebced611c91826d389b7f
aokorodu <aokorodu@gmail.com>
2022-07-02 21:45:20 -0400

    created build, removed build folder from gitignore

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import Wordle from './components/Wordle';
 
 function App() {
+  const getWord = ()=>{
+    let arr = ["Jazzy", "Quack", "Jumbo", "Lunch", "About", "Cards", "Point", "Pizza", "Quick", "Eagle", "Fable", "Facet", "Macho", "Nacho", "Oasis", "Pacer", "Saber", "Vague", "Xenon", "Yacht"];
+    const num = arr.length;
+    const ind = Math.floor(Math.random()*num)
+
+    return arr[ind];
+  }
   return (
-    <Wordle newWord={"LUNCH"} attempts={5}/>
+    <Wordle newWord={getWord()} attempts={5}/>
   );
 }
 
